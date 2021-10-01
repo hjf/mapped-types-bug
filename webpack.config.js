dfcde21c04599db8d76a7593ea2fc3485b40ea81
hjf <hjf@hjf.com.ar>
2021-10-01 13:47:23 -0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = (options, webpack) => {
+  const lazyImports = [
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+  ];
+
+  return {
+    ...options,
+    externals: [],
+    output: {
+      libraryTarget: 'commonjs2',
+    },
+    plugins: [
+      ...options.plugins,
+
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$/,
+      }),
+      // new webpack.IgnorePlugin({
+      //   resourceRegExp: /^aws-sdk$/,
+      // }),
+      // new webpack.IgnorePlugin({
+      //   resourceRegExp: /^aws-lambda$/,
+      // }),
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+    ],
+  };
+};
